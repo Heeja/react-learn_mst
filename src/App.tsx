fbd9b8036a2ee0c9d1c2458e8081dffdaf753fa5
Heeja <holeonline@naver.com>
2023-01-14 01:03:00 +0900

    seted - whileHover, whileTap, using varients, ontapStart, transform-origin

--- src/App.tsx
+++ src/App.tsx
@@ -25,15 +25,26 @@ const FatherDiv = styled(motion.div)`
   width: 100%;
   height: 80%;
 `;
+const AreaBox = styled.div`
+  margin-bottom: 50px;
+`;
 
 const ChildDiv = styled(motion.div)`
-  background-color: rgba(0, 0, 0, 0.3);
-  width: 80%;
-  height: 80%;
+  background-color: rgba(255, 255, 255, 0.5);
+  width: 260px;
+  height: 180px;
+  border-radius: 10px;
 
   display: flex;
   justify-content: center;
   align-items: center;
+
+  &:first-child {
+    transform-origin: bottom right;
+  }
+  &:last-child {
+    transform-origin: top left;
+  }
 `;
 
 const CircleDiv = styled(motion.div)`
@@ -41,6 +52,7 @@ const CircleDiv = styled(motion.div)`
   height: 60px;
   border-radius: 100%;
   background-color: snow;
+  box-shadow: 0px 10px 18px -4px rgb(19, 71, 22);
 `;
 
 const SwitchBtn = styled(motion.button)`
@@ -49,6 +61,8 @@ const SwitchBtn = styled(motion.button)`
   border-radius: 10px;
   background-color: snow;
   border: none;
+
+  font-size: 1.2rem;
 `;
 
 const OverRay = styled(motion.div)`
@@ -63,6 +77,12 @@ const OverRay = styled(motion.div)`
   align-items: center;
 `;
 
+const OverayVarient = {
+  initial: { backgroundColor: "rgba(0,0,0,0)" },
+  animate: { backgroundColor: "rgba(0,0,0,0.8)" },
+  exit: { backgroundColor: "rgba(0,0,0,0)" },
+};
+
 function App() {
   const [circleState, setCirclState] = useState(false);
   const [id, setId] = useState<string | null>("");
@@ -74,25 +94,48 @@ function App() {
   return (
     <>
       <BackgroundDiv>
-        <FatherDiv>
-          <ChildDiv layoutId={"firstBox"} onClick={() => setId("firstBox")} />
-          <ChildDiv>
-            {circleState ? "" : <CircleDiv layoutId="circle" />}
-          </ChildDiv>
-          <ChildDiv>
-            {circleState ? <CircleDiv layoutId="circle" /> : ""}
-          </ChildDiv>
-          <ChildDiv layoutId={"secondBox"} onClick={() => setId("secondBox")} />
-        </FatherDiv>
-        <SwitchBtn onClick={switchClick}>Switch</SwitchBtn>
+        <AreaBox>
+          <FatherDiv>
+            <ChildDiv
+              layoutId={"firstBox"}
+              onClick={() => setId("firstBox")}
+              whileHover={{ scale: 1.2 }}
+            />
+            <ChildDiv>
+              {circleState ? "" : <CircleDiv layoutId="circle" />}
+            </ChildDiv>
+            <ChildDiv>
+              {circleState ? <CircleDiv layoutId="circle" /> : ""}
+            </ChildDiv>
+            <ChildDiv
+              layoutId={"secondBox"}
+              onClick={() => setId("secondBox")}
+              whileHover={{ scale: 1.2 }}
+            />
+          </FatherDiv>
+        </AreaBox>
+        <SwitchBtn
+          onTapStart={switchClick}
+          whileTap={{ scale: [1, 1.3, 1.15] }}
+          style={
+            circleState
+              ? { color: "rgba(222,184,135,1)" }
+              : { color: "rgba(60, 180, 80, 1)" }
+          }
+          transition={{ type: "spring" }}
+        >
+          Switch
+        </SwitchBtn>
       </BackgroundDiv>
       <AnimatePresence>
         {id ? (
           <OverRay
             onClick={() => setId(null)}
-            initial={{ backgroundColor: "rgba(0,0,0,0)" }}
-            animate={{ backgroundColor: "rgba(0,0,0,0.5)" }}
-            exit={{ backgroundColor: "rgba(0,0,0,0)" }}
+            variants={OverayVarient}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{ type: "tween" }}
           >
             <ChildDiv layoutId={id}></ChildDiv>
           </OverRay>
